5d5eea30aa12b05ee4cc4b5e5a451b38fbae36e5
Tako-San <dxe13@mail.ru>
2022-10-02 22:18:18 +0300

    Minor fix

--- drawer/draw.cc
+++ drawer/draw.cc
@@ -8,12 +8,13 @@ sf::RenderWindow GlobalWindow{};
 sf::Event GlobalEvent{};
 
 void init(std::size_t h, std::size_t w) {
-  GlobalWindow.create(sf::VideoMode{w, h}, "Game of life!");
+  GlobalWindow.create(sf::VideoMode(w, h), "Game of life!");
 }
 
 void putPixel(std::size_t y, std::size_t x, std::uint8_t val) {
   auto color = val ? sf::Color::Green : sf::Color::Black;
-  sf::Vertex vert{{x, y}, color};
+  sf::Vector2f coords{static_cast<float>(x), static_cast<float>(y)};
+  sf::Vertex vert{coords, color};
 
   GlobalWindow.draw(&vert, /* vertexCount */ 1, sf::Points);
 }
